c14142130999cf4dd490cf573c81f06ba40b1607
Jeon <jhu82155689@gmail.com>
2017-11-27 02:54:58 +0900

    middle function add

--- main.c
+++ main.c
@@ -19,13 +19,17 @@ int main(void)
 		head = append(head, cur);
 	}
 	printf("입력된 자료의 개수: %d\n\n", count);
+	
 	printf("노드 역순 출력\n");
 	head = reverse(head);
 	printList(head);
-	head = reverse(head);
+	head = reverse(head); //노드 순방향으로 복구 
 
 	printf("노드 순방향 출력\n");
 	printList(head);
+	
+	printf("중간 위치 값의 출력\n");
+	middle(head, count); 
 
 	return 0;
 }
