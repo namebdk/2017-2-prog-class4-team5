fc0fb072f788f67975dc6989979e793cf11c4939
최윤호 <pizza303@naver.com>
2017-11-26 17:03:00 +0900

    메인함수 추가

---
+++ main.c
@@ -0,0 +1,36 @@
+#include "linkedlist.h"
+
+link CreateNode(int value);
+link append(link head, link cur);
+link middle(link head, int count);
+void remove_node(link *head, link p, link removed);
+void remove_oddnode(link *head);
+void reverse(link head);
+void printList(link);
+
+int main(void)
+{
+	link head = NULL;
+	link cur;
+	int number, count = 0;
+
+	while (scanf("%d", &number) != EOF)
+	{
+		count++;
+		cur = CreateNode(number);
+		head = append(head, cur);
+	}
+	printf("입력된 자료의 개수: %d\n\n", count);
+	printf("노드 역순 출력\n");
+	reverse(head);
+	printf("\n중간 위치 노드 출력");
+	middle(head, count);
+	printf("노드 순방향 출력\n");
+	printList(head);
+
+	printf("홀수번째 노드 삭제\n");
+	remove_oddnode(&head);
+	printList(head);
+
+	return 0;
+}
