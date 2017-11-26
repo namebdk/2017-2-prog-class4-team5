c08d1e85cfcb30e14f23fcff5b95ae169e5a4b8f
Youhoseong <h0song@naver.com>
2017-11-26 20:15:10 +0900

    it is right?

---
+++ appendnode.c
@@ -0,0 +1,19 @@
+link append(link head, link cur)
+{
+	link nextNode = head;
+
+	if (head == NULL)
+	{
+		head = cur;
+		return head;
+	}
+
+	while (nextNode->next != NULL)
+	{
+		nextNode = nextNode->next;
+	}
+
+	nextNode->next = cur;
+
+	return head;
+}
