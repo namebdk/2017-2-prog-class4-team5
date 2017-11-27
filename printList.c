17719343a0fbd2cda4793e7a2bdf557a60fa2d7e
Youhoseong <h0song@naver.com>
2017-11-28 00:33:57 +0900

    yunhee kim update

---
+++ printList.c
@@ -0,0 +1,17 @@
+#include "linkedlist.h"
+
+int printList(link head)
+{
+	int cnt = 0;
+	link nextNode = head;
+
+	while (nextNode != NULL)
+	{
+		cnt++;
+		printf("\t%d", nextNode->value);
+		nextNode = nextNode->next;
+	}
+	printf("\n");
+
+	return cnt;
+}
