80fd34d24828d0ed3cceeef7c4872da83aa2e5bc
Youhoseong <h0song@naver.com>
2017-12-03 19:43:34 +0900

    correct main.c

--- Algorithm/main.c
+++ Algorithm/main.c
@@ -2,65 +2,85 @@
 
 int main()
 {
-	char *N, d, CharArrayOfLength = 0;
+	char *CharArrayOfLength, d, N = 0;
 	clock_t start, finish;
 	double duration;
 
+
 	printf("d와 Array size를 입력하세요 :");
-	scanf("%d %d", &d, &CharArrayOfLength);
+	scanf("%d %d", &d, &N);
 
-	N = (char *)malloc(sizeof(char)*CharArrayOfLength);
-	
-	srand(time(NULL));
-	for (int i = 0; i < N; i++)
-	{
-		str[i] = (rand() % 26 + 'A');
-	}
-	str[N] = NULL;
 
-	if (N == NULL)
+	CharArrayOfLength = (char *)malloc(sizeof(char)*N + 1);
+
+	if (CharArrayOfLength == NULL)
 	{
 		printf("ERROR");
 		exit(1);
 	}
-	printf("문자열 랜덤입력 : ");
-	str = (char*)malloc(sizeof(char) * (N + 1));
+
 	srand(time(NULL));
 	for (int i = 0; i < N; i++)
 	{
-		str[i] = (rand() % 26 + 'A');
+		CharArrayOfLength[i] = (rand() % 26 + 'A');
+
 	}
-	str[N] = NULL;
+	CharArrayOfLength[N] = NULL;
+
 
 	start = clock();
-	//trivial solution
+	//trivial solution 
 	finish = clock();
 	duration = (double)(finish - start) / CLOCKS_PER_SEC;
-	printf("문자열 출력 : %s\n",N);
+	printf("문자열 출력 : %s\n", CharArrayOfLength);
 	printf("trivial solution = %f 초입니다.\n", duration);
 
+
 	start = clock();
-	Juggling(N, CharArrayOfLength, d);
+	Juggling(CharArrayOfLength, N, d);
 	finish = clock();
 	duration = (double)(finish - start) / CLOCKS_PER_SEC;
-	printf("문자열 출력 : %s\n", N);
+	printf("문자열 출력 : %s\n", CharArrayOfLength);
 	printf("juggling = %f 초입니다.\n", duration);
 
+
 	start = clock();
-	//blockk-swap
+	//blockk-swap 
 	finish = clock();
 	duration = (double)(finish - start) / CLOCKS_PER_SEC;
-	printf("문자열 출력 : %s\n", N);
+	printf("문자열 출력 : %s\n", CharArrayOfLength);
 	printf("block-swap = %f 초입니다.\n", duration);
 
+
 	start = clock();
-	//reverse
+	//reverse 
 	finish = clock();
 	duration = (double)(finish - start) / CLOCKS_PER_SEC;
-	printf("문자열 출력 : %s\n", N);
+	printf("문자열 출력 : %s\n", CharArrayOfLength);
 	printf("reverse = %f 초입니다.\n", duration);
-
-
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
