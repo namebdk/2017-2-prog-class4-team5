a675a8100770153785c3f7aee92b9c0e73516bc6
Jeon <jhu82155689@gmail.com>
2017-12-03 13:43:20 +0900

    Header file

---
+++ Algorithm/Rotation.h
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+char *trivial(char str[], int n, int d);
+char *juggling(char* str[], int n, int d);
+char *blockswap(char str[], int n, int d);
+char *reverse(char str[], int n, int d);
+
+//필요한 내용들은 보충해주세요 
