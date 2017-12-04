b81a4046451e514828edc8284ce221623714bb8c
sihyun98 <sihyun98@live.cau.ac.kr>
2017-12-04 21:56:12 +0900

    trivial

---
+++ Algorithm/trivial.c
@@ -0,0 +1,24 @@
+void trivial(char str[], int n, int d)
+{
+	char temp = 0;
+	if (d > 0)
+	{
+		for (int i = d; i > 0; i--)
+		{
+			temp = str[0];
+			for (int j = 0; j < n - 1; j++)
+				str[j] = str[j + 1];
+				str[n - 1] = temp;
+		}
+	}
+	else if (d<0)
+	{
+		for (int i = d; i > 0; i--)
+		{
+			temp = str[n - 1];
+			for (int j = n - 1; j > 0; j--)
+				str[j] = str[j - 1];
+			str[0] = temp;
+		}
+	}
+}
